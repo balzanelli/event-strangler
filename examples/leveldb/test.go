fea3ddcbc4c26acc88600914ec6a6ff31cb1b7ae
balzanelli <me@balzanel.li>
2021-10-23 03:49:51 +0100

    test(leveldb): parse test json fixture

--- examples/leveldb/test.go
+++ examples/leveldb/test.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	eventstrangler "github.com/balzanelli/event-strangler"
+	"io/ioutil"
 	"log"
+	"os"
 )
+
+func readJson(fileName string) (*map[string]interface{}, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	var dict map[string]interface{}
+	if err = json.Unmarshal(bytes, &dict); err != nil {
+		return nil, err
+	}
+	return &dict, nil
+}
 
 func main() {
 	log.Println("event-strangler/examples/leveldb@v0.1.0")
@@ -17,9 +36,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	_, err = strangler.Once()
+	dict, err := readJson("test.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("event-strangler@v0.1.0")
+
+	_, err = strangler.Once(*dict)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
